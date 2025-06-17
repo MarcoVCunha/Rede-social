cda667e09eed58ea2e6f8912a9799a96ea32eaf8
MarcoVCunha <126254095+MarcoVCunha@users.noreply.github.com>
2025-06-17 18:03:07 -0300

    register and login

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,19 @@
-const mongoose = require('mongoose') // Import mongoose to interact with MongoDB
-const dbUser = process.env.DB_USER; // Get the database user from environment variables
-const dbPassword = process.env.DB_PASS; // Get the database password from environment variables
+const mongoose = require("mongoose"); // Import mongoose to interact with MongoDB
 
-const conn = async () => { 
-    try {
+const conn = async () => {
+  try {
+    const dbConn = await mongoose.connect(
+        process.env.DB_URI
+    ); // Connect to MongoDB using the connection string with user and password
 
-        const dbConn = await mongoose.connect(
-            `mongodb+srv://${dbUser}:${dbPassword}@cluster2.lwihzcr.mongodb.net/reactgram?retryWrites=true&w=majority&appName=Cluster2`
-        ) // Connect to MongoDB using the connection string with user and password
+    console.log(`MongoDB connected: ${dbConn.connection.host}`); // Log the host of the connected MongoDB instance
 
-        console.log(`MongoDB connected: ${dbConn.connection.host}`); // Log the host of the connected MongoDB instance
-
-        return dbConn;
-    } catch (error) {
-        console.log(error)
-    } // Catch any errors that occur during the connection attempt
-
-}
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  } // Catch any errors that occur during the connection attempt
+};
 
 conn(); // Immediately invoke the connection function to establish the connection when this module is loaded
 
 module.exports = conn; // Export the connection function to be used in other parts of the application
-
