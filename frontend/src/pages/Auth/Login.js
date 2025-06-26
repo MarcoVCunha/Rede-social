8ec26ad5e811e755ef52306ecde41e60d7a13709
MarcoVCunha <126254095+MarcoVCunha@users.noreply.github.com>
2025-06-26 17:47:04 -0300

    form connection register, component message, validate user, hook auth verification, navbar for users authenticated, logout function, logout implementation, login form creation

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,11 +1,47 @@
-import './Auth.css'
+import "./Auth.css";
+
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
 
 const Login = () => {
-  return (
-    <div id='login'>
-      <h1>Login</h1>
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-Mail"
+          onChange={(e) => setEmail(e.value.target)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.value.target)}
+          value={password || ""}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não possui uma conta? <Link to="/register">Clique aqui.</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
